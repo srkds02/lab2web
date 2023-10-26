60b87fe64d3f7df65c09b3fb583e66e88bf1c9a6
Sergey Krasuckis <serkrasuckis@gmail.com>
2023-10-26 23:39:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, done: false }]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
+  const toggleDone = (index) => {
+    const updatedTasks = tasks.map((task, i) =>
+      i === index ? { ...task, done: !task.done } : task
+    );
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo App</h1>
+      <div className="TaskInput">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Новая задача..."
+        />
+        <button onClick={addTask}>Добавить</button>
+      </div>
+      <ul className="TaskList">
+        {tasks.map((task, index) => (
+          <li key={index} className={task.done ? 'done' : ''}>
+            {task.text}
+            <div>
+              <button className="delete" onClick={() => deleteTask(index)}>
+                &#10060;
+              </button>
+              <button className="done" onClick={() => toggleDone(index)}>
+                &#10004;
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
